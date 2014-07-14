0ebfdb57bbe648a1223a326df53e0be2f2f2d7f4
JeromeWu <jeromewus@gmail.com>
2014-07-15 04:55:47 +0800

    modify sel_svmarg.py

--- tools/sel_svmarg.py
+++ tools/sel_svmarg.py
@@ -10,31 +10,39 @@ from lib.svmutil import *
 logger = logging.getLogger( __name__ )
 
 fw_lock = multiprocessing.Lock()
+cnt_lock = multiprocessing.Lock()
+cnt = multiprocessing.Value('i', 0)
+
+
+def inc_cnt():
+  with cnt_lock:
+    cnt.value += 1
+    return cnt.value
 
 def file_write(log_file_name, msg):
   if log_file_name != None:
-    fw_lock.acquire()
-    file_append = open(log_file_name,'a')
-    file_append.write(msg)
-    file_append.close()
-    fw_lock.release()
+    with fw_lock:
+      file_append = open(log_file_name,'a')
+      file_append.write(msg)
+      file_append.close()
 
-def worker_in(train_y, train_x, args, log_file_name, out_queue):
+def worker_in(train_y, train_x, args, log_file_name):
   args_str = ('-q -v %i -c %f -g %f') % (args['n_folds'], args['c'], args['g'])
   acc = svm_train(train_y, train_x, args_str)
-  out_queue.put({'acc':acc, 'c':args['c'], 'g':args['g']})
-  logger.info(('jobs finished: %i/%i, acc=%f, c=%f, g=%f') % (out_queue.qsize(), args['n_jobs'], acc, args['c'], args['g']))
+  logger.info(('jobs finished: %i/%i, acc=%f, c=%f, g=%f') % (inc_cnt(), args['n_jobs'], acc, args['c'], args['g']))
   file_write(log_file_name, ('%f %f %f') % (acc, args['c'], args['g']))
+  return {'acc':acc, 'c':args['c'], 'g':args['g']}
 
-def worker_out(train_y, train_x, test_y, test_x, args, log_file_name, out_queue):
+def worker_out(train_y, train_x, test_y, test_x, args, log_file_name):
   args_str = ('-q -c %f -g %f') % (args['c'], args['g'])
   model = svm_train(train_y, train_x, args_str)
   label, (acc, mse, scc), val = svm_predict(test_y, test_x, model)
-  out_queue.put({'acc':acc, 'c':args['c'], 'g':args['g']})
-  logger.info(('jobs finished: %i/%i, acc=%f, c=%f, g=%f') % (out_queue.qsize(), args['n_jobs'], acc, args['c'], args['g']))
+  logger.info(('jobs finished: %i/%i, acc=%f, c=%f, g=%f') % (inc_cnt(), args['n_jobs'], acc, args['c'], args['g']))
   file_write(log_file_name, ('%f %f %f') % (acc, args['c'], args['g']))
+  return {'acc':acc, 'c':args['c'], 'g':args['g']}
 
 def sel_svmarg_in(train_file_name=None, log_file_name=None, n_folds=0, c_start=-5, c_end=16, c_step=2, g_start=3, g_end=-16, g_step=-2):
+  pool = multiprocessing.Pool()
   logger.info('loading files')
   train_y, train_x = svm_read_problem(train_file_name)
   logger.info('start selecting arguments')
@@ -45,40 +53,34 @@ def sel_svmarg_in(train_file_name=None, log_file_name=None, n_folds=0, c_start=-
   for c in c_list:
     for g in g_list:
       args = {'n_folds':n_folds, 'c':pow(2, c), 'g':pow(2, g), 'n_jobs':len(c_list)*len(g_list)}
-      p = multiprocessing.Process(target=worker_in, args=(train_y, train_x, args, log_file_name, out_queue))
+      p = pool.apply_async(worker_in, (train_y, train_x, args, log_file_name, ))
       jobs.append(p)
-      p.start()
-  for p in jobs:
-    p.join()
-
+ 
   ret = []
-  while out_queue.empty() != True:
-    ret.append(out_queue.get())
-  
+  for i in xrange(len(jobs)):
+    ret.append(jobs[i].get())
+
   max_ret = max(ret, key=lambda k:k['acc'])
   logger.info(('max acc=%f, c=%f, g=%f') % (max_ret['acc'], max_ret['c'], max_ret['g']))
 
 def sel_svmarg_out(train_file_name=None, test_file_name=None, log_file_name=None, c_start=-5, c_end=16, c_step=2, g_start=3, g_end=-16, g_step=-2):
+  pool = multiprocessing.Pool()
   logger.info('loading files')
   train_y, train_x = svm_read_problem(train_file_name)
   test_y, test_x = svm_read_problem(test_file_name)
   logger.info('start selecting arguments')
-  out_queue = multiprocessing.Queue()
   jobs = []
   c_list = xrange(c_start, c_end, c_step)
   g_list = xrange(g_start, g_end, g_step)
   for c in c_list:
     for g in g_list:
       args = {'c':pow(2, c), 'g':pow(2, g), 'n_jobs':len(c_list)*len(g_list)}
-      p = multiprocessing.Process(target=worker_out, args=(train_y, train_x, test_y, test_x, args, log_file_name, out_queue))
+      p = pool.apply_async(worker_out, (train_y, train_x, test_y, test_x, args, log_file_name, ))
       jobs.append(p)
-      p.start()
-  for p in jobs:
-    p.join()
-
+  
   ret = []
-  while out_queue.empty() != True:
-    ret.append(out_queue.get())
+  for i in xrange(len(jobs)):
+    ret.append(jobs[i].get())
   
   max_ret = max(ret, key=lambda k:k['acc'])
   logger.info(('max acc=%f, c=%f, g=%f') % (max_ret['acc'], max_ret['c'], max_ret['g']))
